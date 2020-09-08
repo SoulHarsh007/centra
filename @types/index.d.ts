d2a6b6c6cf154291db2a9e01aac3b00a80f27bf5
SoulHarsh007 <harshtheking@hotmail.com>
2020-09-08 20:11:13 +0530

    Init Repo

---
+++ @types/index.d.ts
@@ -0,0 +1,41 @@
+/// <reference types='node'>
+import {RequestOptions, IncomingMessage} from 'http';
+import {URL} from 'url';
+interface CentraInit {
+  (url: URL | string, method?: string): Centra.Request;
+}
+declare const Centra: CentraInit;
+declare namespace Centra {
+  interface Response {
+    coreRes: IncomingMessage;
+    headers: IncomingMessage['headers'];
+    statusCode: IncomingMessage['statusCode'];
+    body: Buffer;
+    json(): Promise<any>;
+    text(): Promise<string>;
+  }
+  interface Request {
+    url: URL;
+    httpMethod: string;
+    data: string | Buffer | null;
+    sendDataAs: 'form' | 'json' | 'buffer' | null;
+    reqHeaders: {[k: string]: string};
+    streamEnabled: boolean;
+    compressionEnabled: boolean;
+    coreOptions: RequestOptions;
+    query(key: string, value: any): this;
+    query(params: {[k: string]: any}): this;
+    path(relativePath: string): this;
+    body(data: any, sendAs?: 'json' | 'buffer' | 'form'): this;
+    header(key: string, value: string): this;
+    header(headers: {[k: string]: string}): this;
+    method(method: string): this;
+    timeout(timeMs: number): this;
+    send(): Promise<Response>;
+    json(): Promise<Response['json']>;
+    raw(): Promise<Response['body']>;
+    text(): Promise<Response['text']>;
+  }
+}
+
+export = Centra;
---
+++ README.md
@@ -0,0 +1,8 @@
+# Centra-Aero
+
+Fork of centra to support Request#method, and remove unneeded clutter.
+
+`npm i @nia3208/centra`
+
+Forked from @aero/centra: <https://git.farfrom.earth/aero/forks/centra.git>
+Original by Ethan Davis: <https://github.com/ethanent/centra>
